83fe05cdd14eb3852cbcca75130bcf78f81970b1
Cornelia Franta <franta-cornelia@gmx.at>
2020-06-25 13:36:29 +0200

    setting up folder

--- myBlog/src/app/posts.ts
+++ myBlog/src/app/posts.ts
@@ -1,8 +1,85 @@
 export const posts = [
   {
     buttonPressed: '0',
+    index: '3',
+    title: 'Chocolaty Dream',
+    subtitle: 'Fluffy Chocolate cake filled with a creamy Chocolate filling',
+    img: 'chocolateCake.jpg',
+    dateHrf: '27.06.2020',
+    timeHrf: '14:00',
+    content: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy' +
+      ' eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.' +
+      ' At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, ' +
+      'no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, ' +
+      'consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore' +
+      'magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.' +
+      ' Stet clita kasd gubergren,' +
+      ' no sea takimata sanctus est Lorem ipsum dolor sit amet.',
+    author: 'Cornelia',
+    location: 'Vienna',
+    likes: '0',
+  }, {
+    buttonPressed: '0',
+    index: '3',
+    title: 'A Autumny Barbecue',
+    subtitle: 'Pumpkin and Sellery Mash with glaced barbeque skewers',
+    img: 'pumpkinPuree.jpg',
+    dateHrf: '26.06.2020',
+    timeHrf: '20:00',
+    content: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy' +
+      ' eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.' +
+      ' At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, ' +
+      'no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, ' +
+      'consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore' +
+      'magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.' +
+      ' Stet clita kasd gubergren,' +
+      ' no sea takimata sanctus est Lorem ipsum dolor sit amet.',
+    author: 'Hans-Jürgen',
+    location: 'Vienna',
+    likes: '0',
+  }, {
+    buttonPressed: '0',
+    index: '4',
+    title: 'Like Holidays',
+    subtitle: 'Baked Zuchini filled with dried tomatoes, pine seeds and basil, with garlicy mashed potatoes',
+    img: 'stuffed-zuccini.jpg',
+    dateHrf: '26.06.2020',
+    timeHrf: '15:00',
+    content: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy' +
+      ' eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.' +
+      ' At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, ' +
+      'no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, ' +
+      'consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore' +
+      'magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.' +
+      ' Stet clita kasd gubergren,' +
+      ' no sea takimata sanctus est Lorem ipsum dolor sit amet.',
+    author: 'Balint',
+    location: 'Vienna',
+    likes: '0',
+  }, {
+    buttonPressed: '0',
+    index: '3',
+    title: 'Typical Sweden',
+    subtitle: 'Vegan Köttbullar with creamy mashed potatoes',
+    img: 'köttbullar.jpg',
+    dateHrf: '25.06.2020',
+    timeHrf: '19:00',
+    content: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy' +
+      ' eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.' +
+      ' At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, ' +
+      'no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, ' +
+      'consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore' +
+      'magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.' +
+      ' Stet clita kasd gubergren,' +
+      ' no sea takimata sanctus est Lorem ipsum dolor sit amet.',
+    author: 'Cornelia',
+    location: 'Vienna',
+    likes: '0',
+  }, {
+    buttonPressed: '0',
     index: '2',
     title: 'Summer is commming',
+    subtitle: 'Strawberry Rhubarb Tarte with Coconut-Vanilla Cream',
     img: 'strawberrytarte.jpg',
     dateHrf: '25.06.2020',
     timeHrf: '16:00',
@@ -21,15 +98,16 @@ export const posts = [
     buttonPressed: '0',
     index: '1',
     title: 'A rainbow for breakfast',
+    subtitle: 'A healthy start in the Day',
     img: 'breakfast.jpg',
     dateHrf: '24.06.2020',
     timeHrf: '08:00',
-    content: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, +\n' +
-      '      sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, +\n' +
-      '       sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.  +\n' +
-      '      Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. +\n' +
-      '      Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt +\n' +
-      '       ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo  +\n' +
+    content: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, ' +
+      '      sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, ' +
+      '       sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.  ' +
+      '      Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. ' +
+      '      Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ' +
+      '       ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo ' +
       '      dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.,',
     author: 'Balint',
     location: 'Vienna',
@@ -38,6 +116,7 @@ export const posts = [
     buttonPressed: '0',
     index: '0',
     title: 'Welcome',
+    subtitle: 'A Blog about delicious vegan food',
     img: 'welcome.jpg',
     dateHrf: '23.06.2020',
     timeHrf: '10:00',
